95920110a353a6f59ab13988430ae9ed7e66b659
Anthony Jackson <ajackson@spartaglobal.com>
2018-08-19 13:50:00 +0100

    tidy up some code

--- js/index.js
+++ js/index.js
@@ -1,228 +1,235 @@
-$(function(event) {
-  // Set up Outer Scoped Variables to be manipulated by the functions
-  // ****************************************************************
+$(function (event) {
+    // Set up Outer Scoped Variables to be manipulated by the functions
+    // ****************************************************************
 
-  // Rows and columns count
-  var dimension = 0;
+    // Rows and columns count
+    var dimension = 0;
 
-  var state = {blank:"0", x:"X", o:"O"};
-  var playersTurn = true;
+    var state = { blank: "0", x: "X", o: "O" };
+    var playersTurn = state.x;
 
-  var board = [];
-  var moveCount = 0;
+    var board = [];
+    var moveCount = 0;
 
-  //event listener for get dimentios button
-  $("#submit").off().on("click", getDimension);
+    //event listener for get dimentios button
+    $("#submit").off().on("click", getDimension);
 
-  //Define functions below
-  //**********************
+    //Define functions below
+    //**********************
 
-  function getDimension(event) {
-    dimension = parseInt($("#dimention").val());
+    function getDimension(event) {
+        dimension = parseInt($("#dimention").val());
 
-    //initialise board.
-    setupBoard(dimension);
-  };
+        //initialise board.
+        setupBoard(dimension);
+    };
 
-  //Fill out 2 dimensional board array
-  function setupBoard(dimension) {
-    var rowDiv ="", colDiv ="", gridAdd ="";
-    var colClass="", rowClass="";
+    //Fill out 2 dimensional board array
+    function setupBoard(dimension) {
+        var rowDiv = "", colDiv = "", gridAdd = "";
+        var colClass = "", rowClass = "";
 
-    for (var row = 0; row < dimension; row++) {
-      board[row] = [];
-      rowClass = "row"+row;
-      rowDiv = '<div class="row '+rowClass+'">'
-      for (var column = 0; column < dimension; column++) {
-        board[row][column] = state.blank;
-        colClass = "col"+column;
-        colDiv = '<div class="'+colClass+' '+rowClass+' innerSquare col-xs-1">.</div>'
-        rowDiv += colDiv
-      }
-      rowDiv += '</div>';
-      gridAdd += rowDiv;
-    }
-    //build HTML grid
-    $(".grid").html(gridAdd);
-
-    //set centering offset
-    $(".grid").removeClass("col-xs-offset-4")
-    var offset = parseInt( (dimension < 12)? ((12-dimension)/2) : 0 )
-    if(offset > 0)
-      $(".grid").addClass("col-xs-offset-"+offset);
-
-    //add an Event listener to each innerSquare class
-    $(".innerSquare").off().on("click", processMove);
-  };
-
-  //check end conditions functions
-  function checkColumnWin(dimension, columnIndex, newState) {
-    //Check for a win in current Column
-    for(var i = 0; i < dimension; i++){
-        if(board[columnIndex][i] != newState)
-            break;
-        if(i == dimension-1){
-            //report win for squareState
-            console.log("win for: " + newState);
-            return "win for: " + newState;
+        for (var row = 0; row < dimension; row++) {
+            board[row] = [];
+            rowClass = "row" + row;
+            rowDiv = '<div class="row ' + rowClass + '">'
+            for (var column = 0; column < dimension; column++) {
+                board[row][column] = state.blank;
+                colClass = "col" + column;
+                colDiv = '<div class="' + colClass + ' ' + rowClass + ' innerSquare col-xs-1">.</div>'
+                rowDiv += colDiv
+            }
+            rowDiv += '</div>';
+            gridAdd += rowDiv;
         }
-    }
-    // No win yet
-    return "";
-  }
+        //build HTML grid
+        $(".grid").html(gridAdd);
 
-  function checkRowWin(dimension, rowIndex, newState) {
-    //Check for a win in current Row
-    for(var i = 0; i < dimension; i++){
-        if(board[i][rowIndex] != newState)
-            break;
-        if(i == dimension-1){
-            //report win for squareState
-            console.log("win for: " + newState);
-            return "win for: " + newState;
-        }
-    }
-    // No win yet
-    return "";
-  }
+        //set centering offset
+        $(".grid").removeClass("col-xs-offset-4")
+        var offset = parseInt((dimension < 12) ? ((12 - dimension) / 2) : 0)
+        if (offset > 0)
+            $(".grid").addClass("col-xs-offset-" + offset);
 
-  function checkDiagonalWin(dimension, columnIndex, rowIndex, newState) {
-    //Check for a win on the diagonal
-    if(columnIndex == rowIndex){
-        //we're on a diagonal
-        for(var i = 0; i < dimension; i++){
-            if(board[i][i] != newState)
+        //add an Event listener to each innerSquare class
+        $(".innerSquare").off().on("click", processMove);
+    };
+
+    //check end conditions functions
+    function checkColumnWin(dimension, columnIndex, newState) {
+        //Check for a win in current Column
+        for (var i = 0; i < dimension; i++) {
+            if (board[columnIndex][i] != newState)
                 break;
-            if(i == dimension-1){
-                //report win for newState
+            if (i == dimension - 1) {
+                //report win for squareState
                 console.log("win for: " + newState);
                 return "win for: " + newState;
             }
         }
+        // No win yet
+        return "";
     }
-    // No win yet
-    return "";
-  }
 
-  function checkAntiDiagonalWin(dimension, columnIndex, rowIndex, newState) {
-    //Check for a win on the anti-diagonal
-    if(columnIndex + rowIndex == dimension - 1){
-        for(var i = 0; i < dimension; i++){
-            if(board[i][(dimension-1)-i] != newState)
+    function checkRowWin(dimension, rowIndex, newState) {
+        //Check for a win in current Row
+        for (var i = 0; i < dimension; i++) {
+            if (board[i][rowIndex] != newState)
                 break;
-            if(i == dimension-1){
-                //report win for newState
+            if (i == dimension - 1) {
+                //report win for squareState
                 console.log("win for: " + newState);
                 return "win for: " + newState;
             }
         }
+        // No win yet
+        return "";
     }
-    // No win yet
-    return "";
-  }
 
-  function checkForDraw(dimension, moveCount) {
-    //Check for a draw
-    if(moveCount == (Math.pow(dimension, 2) - 1)){
-        //report draw
-        console.log("This round was a draw");
-        return "This round was a draw";
+    function checkDiagonalWin(dimension, columnIndex, rowIndex, newState) {
+        //Check for a win on the diagonal
+        if (columnIndex == rowIndex) {
+            //we're on a diagonal
+            for (var i = 0; i < dimension; i++) {
+                if (board[i][i] != newState)
+                    break;
+                if (i == dimension - 1) {
+                    //report win for newState
+                    console.log("win for: " + newState);
+                    return "win for: " + newState;
+                }
+            }
+        }
+        // No win yet
+        return "";
     }
-    // No draw yet continue game.
-    return "";
-  }
 
-  function checkForWin(columnIndex, rowIndex, newState) {
-    moveCount++;
-    var result = "";
-    //check end conditions
+    function checkAntiDiagonalWin(dimension, columnIndex, rowIndex, newState) {
+        //Check for a win on the anti-diagonal
+        if (columnIndex + rowIndex == dimension - 1) {
+            for (var i = 0; i < dimension; i++) {
+                if (board[i][(dimension - 1) - i] != newState)
+                    break;
+                if (i == dimension - 1) {
+                    //report win for newState
+                    console.log("win for: " + newState);
+                    return "win for: " + newState;
+                }
+            }
+        }
+        // No win yet
+        return "";
+    }
 
-    //check current Column for a win
-    result = checkColumnWin(dimension, columnIndex, newState);
-    if (result != "") {
-        //A win has occured
+    function checkForDraw(dimension, moveCount) {
+        //Check for a draw
+        if (moveCount == (Math.pow(dimension, 2) - 1)) {
+            //report draw
+            console.log("This round was a draw");
+            return "This round was a draw";
+        }
+        // No draw yet continue game.
+        return "";
+    }
+
+    function checkForWin(columnIndex, rowIndex, newState) {
+        moveCount++;
+        var result = "";
+        //check end conditions
+
+        //check current Column for a win
+        result = checkColumnWin(dimension, columnIndex, newState);
+        if (result != "") {
+            //A win has occured
+            return result;
+        }
+
+        //check current Row for a win
+        result = checkRowWin(dimension, rowIndex, newState);
+        if (result != "") {
+            //A win has occured
+            return result;
+        }
+
+        //check Diagonal for a win
+        result = checkDiagonalWin(dimension, columnIndex, rowIndex, newState);
+        if (result != "") {
+            //A win has occured
+            return result;
+        }
+
+        //check anti diagonal for a win
+        result = checkAntiDiagonalWin(dimension, columnIndex, rowIndex, newState);
+        if (result != "") {
+            //A win has occured
+            return result;
+        }
+
+
+        //check if game has reached a draw
+        result = checkForDraw(dimension, moveCount);
+        if (result != "") {
+            //A draw has occured
+            return result;
+        }
+
+        //No win or draw yet continue game.
         return result;
-    }
+    };
 
-    //check current Row for a win
-    result = checkRowWin(dimension, rowIndex, newState);
-    if (result != "") {
-        //A win has occured
-        return result;
-    }
+    //Handle turns, get grid references, set grid state and check For A Win
+    function processMove(event) {
+        var moveColumn = -1;
+        var moveRow = -1;
+        var newState = state.blank;
 
-    //check Diagonal for a win
-    result = checkDiagonalWin(dimension, columnIndex, rowIndex, newState);
-    if (result != "") {
-        //A win has occured
-        return result;
-    }
+        if (playersTurn == state.x) {
+            //set state for board square
+            newState = state.x;
 
-    //check anti diagonal for a win
-    result = checkAntiDiagonalWin(dimension, columnIndex, rowIndex, newState);
-    if (result != "") {
-        //A win has occured
-        return result;
-    }
+            //switch players for next turn
+            playersTurn = state.o;
+        }
+        else if (playersTurn == state.o) {
+            //set state for board square
+            newState = state.o;
 
+            //switch players for next turn
+            playersTurn = state.x;
+        }
 
-    //check if game has reached a draw
-    result = checkForDraw(dimension, moveCount);
-    if (result != "") {
-        //A draw has occured
-        return result;
-    }
+        var square = event.currentTarget;
 
-    //No win or draw yet continue game.
-    return result;
-  };
+        var columnString;
+        var rowString;
+        for (var i = 0; i < dimension; i++) {
+            columnString = "col" + i.toString();
+            if (square.classList[0] == columnString) {
+                moveColumn = i;
+            }
 
-  //Handle turns, get grid references, set grid state and check For A Win
-  function processMove(event) {
-    var moveColumn = -1;
-    var moveRow = -1;
-    var newState = state.blank;
+            rowString = "row" + i.toString();
+            if (square.classList[1] == rowString) {
+                moveRow = i;
+            }
 
-    if(playersTurn == true )
-    {
-      newState = state.x;
-    }
-    else {
-      newState = state.o;
-    }
-    playersTurn = !playersTurn
+            if (moveColumn != -1 && moveRow != -1)
+                break;
+        }
 
-    var square = event.currentTarget;
+        //Only record move if its for an empty square
+        if (board[moveColumn][moveRow] == state.blank) {
+            var result = "";
+            board[moveColumn][moveRow] = newState;
+            //set display for square to X or O
+            square.innerText = newState; //firstElementChild.
 
-    var columnString;
-    var rowString;
-    for (var i = 0; i < dimension; i++) {
-      columnString = "col"+i.toString();
-      if (square.classList[0] == columnString) {
-          moveColumn = i;
-      }
+            result = checkForWin(moveColumn, moveRow, newState);
+            if (result != "") {
+                $(".winner").html(result);
+            }
+        }
 
-      rowString = "row"+i.toString();
-      if (square.classList[1] == rowString) {
-          moveRow = i;
-      }
-
-      if (moveColumn != -1 && moveRow != -1)
-        break;
-    }
-
-    //Only record move if its for an empty square
-    if(board[moveColumn][moveRow] == state.blank){
-        var result="";
-        board[moveColumn][moveRow] = newState;
-        //set display for square to X or O
-        square.innerText = newState; //firstElementChild.
-
-        result = checkForWin(moveColumn, moveRow, newState);
-        if (result != "")
-          alert(result);
-    }
-
-  };
+    };
 
 });
